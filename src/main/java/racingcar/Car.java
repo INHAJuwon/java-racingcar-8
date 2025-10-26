7b51cf1122fb621f74ff5c74d1986af8cc00f81b
ChoiJuwon <aowlrdofhdn@naver.com>
2025-10-26 20:17:03 +0900

    feat(racingcar): 각 자동차 전진을 행하는 Car 및 Moving 인터페이스 추가, Splitter 수정
    - Moving: 자동차의 전진 가능 여부를 결정하는 규칙을 정의하는 인터페이스
    - Car: 이름, 위치 등 각 자동차를 표현한다.
    - Splitter: 기존 Map에서 List<Car>로 변경한다.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+public class Car {
+    private int position;
+    private final String name;
+    private final Moving moving;
+
+    public Car(String name, Moving moving) {
+        this.name = name;
+        this.moving = moving;
+        position=0;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void moveForward() {
+        if(moving.canMove()){
+            this.position++;
+        }
+    }
+}
--- src/main/java/racingcar/Splitter.java
+++ src/main/java/racingcar/Splitter.java
@@ -1,25 +1,32 @@
 package racingcar;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Splitter {
-    final static String delimiter = ",";
+    private final static String delimiter = ",";
+    private final Moving moving;
 
-    public Map<String, Integer> convertMap(String cars) {
-        Map<String, Integer> carMap = new LinkedHashMap<>();
-        String[] splitter= cars.split(delimiter);
-
-        for (String car : splitter) {
-            checkLength(car);
-            carMap.put(car, 0);
-        }
-
-        return carMap;
+    public Splitter(Moving moving){
+        this.moving=moving;
     }
 
-    private void checkLength(String car){
-        if(car.length()>5)
+    public List<Car> convertList(String cars) {
+        List<Car> carList = new ArrayList<>();
+        String[] splitter = cars.split(delimiter);
+
+        for (String car : splitter) {
+            car=car.trim();
+            checkLength(car);
+            carList.add(new Car(car,moving));
+        }
+
+        return carList;
+    }
+
+    private void checkLength(String car) {
+        if (car.length() > 5)
             throw new IllegalArgumentException("이름 길이 5 초과");
     }
 }
