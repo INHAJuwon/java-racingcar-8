bd8825cfff0ac4143771d364b9eb23a0f4533e29
ChoiJuwon <aowlrdofhdn@naver.com>
2025-10-26 20:31:58 +0900

    feat(racingcar): 경주 객체 통합 구현
    - 모든 게임 클래스 객체를 생성하고 전체 자동차 경주의 실행 흐름을 연결한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,19 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input input=new Input();
+        Moving moving=new RandomValueCalculator();
+        Splitter splitter=new Splitter(moving);
+        String cars=input.readCars();
+        CarDTO carsDTO=new CarDTO(splitter.convertList(cars));
+        Output output=new Output();
+        Winner winners=new Winner();
+
+
+        int count= input.readCount();
+
+        GameManager manager=new GameManager(output,winners);
+
+        manager.run(carsDTO,count);
     }
 }
